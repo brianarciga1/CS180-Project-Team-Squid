0aa81d93fa6b5d3425c4564e830d568845f081a3
cooled909 <esegu007@ucr.edu>
2023-03-12 20:18:59 -0700

    working on docker

--- aniplaylist/vite.config.js
+++ aniplaylist/vite.config.js
@@ -8,12 +8,6 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: "127.0.0.1",
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true
-      }
-    }
   },
   resolve: {
     alias: {
